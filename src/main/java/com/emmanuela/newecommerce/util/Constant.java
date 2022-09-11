1ac5c88acd492a355a7aa489ad045fc423675efe
DevEmmanuela <ikeemmanuelachinwe@gmail.com>
2022-09-11 13:58:50 +0100

    feat/User Registration

--- src/main/java/com/emmanuela/newecommerce/util/Constant.java
+++ src/main/java/com/emmanuela/newecommerce/util/Constant.java
@@ -2,4 +2,6 @@ package com.emmanuela.newecommerce.util;
 
 public class Constant {
     public static final String KEYS = "secret";
+
+    //public static final String EMAIL_VERIFICATION_LINK = "http://localhost:4545/api/v1/confirmToken?token=";
 }
